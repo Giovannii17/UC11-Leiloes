f7193fe8ed0f56cff0534142902d6ffc5e454a6f
Giovanni <giovannisilvasantos173@gmail.com>
2024-01-05 16:42:22 -0300

    Adicionado funcionalidade de salvar itens no banco de dados

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -23,10 +23,40 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
+        conn = new conectaDAO().connectDB();
+
+    try {
+        // Preparando a query SQL para inserção de um novo produto
+        String query = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+        prep = conn.prepareStatement(query);
+
+        // Configurando os parâmetros da query com os dados do produto
+        prep.setString(1, produto.getNome());
+        prep.setInt(2, produto.getValor());
+        prep.setString(3, produto.getStatus());
+
+        // Executando a query para inserir o produto no banco de dados
+        prep.executeUpdate();
+
+        JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+
+    } catch (Exception e) {
+        e.printStackTrace();
+        JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto.");
+    } finally {
+        // Fechando recursos (é importante liberar recursos para evitar vazamentos)
+        try {
+            if (prep != null) {
+                prep.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+     
         
     }
     
